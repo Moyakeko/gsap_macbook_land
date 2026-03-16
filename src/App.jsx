0ec3a8f25c6c6f1c0db8cffeb4f96a4c12734fd4
Moyakeko <119612454+Moyakeko@users.noreply.github.com>
2026-03-16 19:10:28 +0100

    Implement switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/productViewer.jsx";
+import {ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
